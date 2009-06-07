5c0d80f35544502470886dbfb6b424add19bc117
Mathis Hofer <mathis@fsfe.org>
2009-06-07 21:11:02 +0200

    code styling (PEP 8)

--- feeds2imap.py
+++ feeds2imap.py
@@ -28,11 +28,21 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
 
+import sys
+import os
+import time
+import string
+import optparse
+
+import urllib2
+import imaplib
+import feedparser
+import modutf7
+import email
+
 import xml.dom.minidom
 from xml.dom.minidom import Node
 from xml import xpath
-
-import sys, os, time, string, optparse, urllib2, imaplib, feedparser, modutf7, email
 
 
 class Feed:
@@ -41,76 +51,75 @@ class Feed:
         self.mailbox = mailbox
         self.data = None
         
-    def hasData(self):
-        return self.data != None
+    def has_data(self):
+        return self.data is not None
 
 
 class FeedReader:
-    def __init__(self, feeds, verbose, imapServer, imapSSL, imapPort, imapUser, imapPwd, msgLimit):
+    def __init__(self, feeds, verbose, imap_server, imap_ssl, imap_port, imap_user, imap_pwd, msg_limit):
         self.feeds = feeds
         self.verbose = verbose
-        self.imapServer = imapServer
-        self.imapSSL = imapSSL
-        self.imapPort = imapPort
-        self.imapUser = imapUser
-        self.imapPwd = imapPwd
-        self.msgLimit = msgLimit
+        self.imap_server = imap_server
+        self.imap_ssl = imap_ssl
+        self.imap_port = imap_port
+        self.imap_user = imap_user
+        self.imap_pwd = imap_pwd
+        self.msg_limit = msg_limit
     
     def start(self):
-        if not self.imapSSL:
-            self.imapConn = imaplib.IMAP4(self.imapServer, self.imapPort)
+        if not self.imap_ssl:
+            self.imap_conn = imaplib.IMAP4(self.imap_server, self.imap_port)
         else:
-            self.imapConn = imaplib.IMAP4_SSL(self.imapServer, self.imapPort)
-        self.imapConn.login(self.imapUser, self.imapPwd)
+            self.imap_conn = imaplib.IMAP4_SSL(self.imap_server, self.imap_port)
+        self.imap_conn.login(self.imap_user, self.imap_pwd)
         
         for feed in self.feeds:
-            self.__checkFeedMailbox(feed.mailbox)
-            date = self.__getNewestMessageDate(feed.mailbox, feed.url)
+            self.__check_feed_mailbox(feed.mailbox)
+            date = self.__get_newest_message_date(feed.mailbox, feed.url)
             if verbose:
                 print 'Fetching %s ...' % feed.url,
-            newArticles = 0
-            updatedArticles = 0
+            new_articles = 0
+            updated_articles = 0
             sys.stdout.flush()
             try:
                 f = urllib2.urlopen(feed.url)
                 try:
-                    feedData = feedparser.parse(f.read())
-                    feed.data = feedData
+                    feed.data = feedparser.parse(f.read())
                     for entry in feed.data.entries:
                         try:
-                            if (not entry.has_key('updated_parsed') or entry.updated_parsed == None
-                                or time.mktime(entry.updated_parsed) > time.mktime(date)):
-                                mid = self.__getMessageId(feed.mailbox, entry.link)
-                                if mid == None:
+                            if not entry.has_key('updated_parsed') or entry.updated_parsed is None \
+                              or time.mktime(entry.updated_parsed) > time.mktime(date):
+                                mid = self.__get_message_id(feed.mailbox, entry.link)
+                                if mid is None:
                                     # Create new entry
-                                    self.__createMimeMessage(feed, entry)
+                                    self.__create_mime_message(feed, entry)
                                     
-                                    newArticles += 1
-                                elif entry.has_key('updated_parsed') and entry.updated_parsed != None:
+                                    new_articles += 1
+                                elif entry.has_key('updated_parsed') and entry.updated_parsed is not None:
                                     # Entry has been updated, read date of old entry
-                                    old_msg = self.__checkImapResult(self.imapConn.fetch(mid, '(RFC822)'))
+                                    old_msg = self.__check_imap_result(self.imap_conn.fetch(mid, '(RFC822)'))
                                     old_msg_parsed = email.message_from_string(old_msg[0][1])
                                     created_date = old_msg_parsed['DATE']
                                     last_updated_date = old_msg_parsed.has_key('X-FEED-LASTUPDATED') and old_msg_parsed['X-FEED-LASTUPDATED'] or None
                                     
-                                    if (last_updated_date == None or
-                                        entry.updated_parsed > time.strptime(last_updated_date+' UTC', "%a, %d %b %Y %H:%M:%S +0000 %Z")):
+                                    if last_updated_date is None or \
+                                      entry.updated_parsed > time.strptime(last_updated_date+' UTC', "%a, %d %b %Y %H:%M:%S +0000 %Z"):
                                         # Delete old entry
-                                        self.__checkImapResult(self.imapConn.select(feed.mailbox.encode('mod-utf-7')))
-                                        self.__checkImapResult(self.imapConn.store(mid, '+FLAGS', '\\Deleted'))
-                                        self.__checkImapResult(self.imapConn.select(feed.mailbox.encode('mod-utf-7'), True))
+                                        self.__check_imap_result(self.imap_conn.select(feed.mailbox.encode('mod-utf-7')))
+                                        self.__check_imap_result(self.imap_conn.store(mid, '+FLAGS', '\\Deleted'))
+                                        self.__check_imap_result(self.imap_conn.select(feed.mailbox.encode('mod-utf-7'), True))
                                         
                                         # Create new entry with old date
-                                        self.__createMimeMessage(feed, entry, created_date)
+                                        self.__create_mime_message(feed, entry, created_date)
                                         
-                                        updatedArticles += 1
+                                        updated_articles += 1
                         except Exception, e:
                             if verbose:
                                 print 'Error!'
                             print >> sys.stderr, 'Failed to store entry of feed %s to IMAP server:' % feed.url
                             print >> sys.stderr, e
                     try:
-                        self.__cleanFeedMailbox(feed.mailbox)
+                        self.__clean_feed_mailbox(feed.mailbox)
                     except Exception, e:
                         if verbose:
                             print 'Error!'
@@ -129,54 +138,54 @@ class FeedReader:
                 print >> sys.stderr, e
             else:
                 if verbose:
-                    if newArticles + updatedArticles > 0:
-                        print "(%s new, %s updated articles)" % (newArticles, updatedArticles)
+                    if new_articles + updated_articles > 0:
+                        print "(%s new, %s updated articles)" % (new_articles, updated_articles)
                     else:
                         print "(nothing new)"
         
-        self.imapConn.logout()
+        self.imap_conn.logout()
 
-    def __checkFeedMailbox(self, mailbox):
-        (response, data) = self.imapConn.select(mailbox.encode('mod-utf-7'), True)
+    def __check_feed_mailbox(self, mailbox):
+        (response, data) = self.imap_conn.select(mailbox.encode('mod-utf-7'), True)
         if response == 'NO':
-            self.__checkImapResult(self.imapConn.create(mailbox.encode('mod-utf-7')))
-            self.__checkImapResult(self.imapConn.subscribe(mailbox.encode('mod-utf-7')))
-            self.__checkImapResult(self.imapConn.select(mailbox.encode('mod-utf-7'), True))
+            self.__check_imap_result(self.imap_conn.create(mailbox.encode('mod-utf-7')))
+            self.__check_imap_result(self.imap_conn.subscribe(mailbox.encode('mod-utf-7')))
+            self.__check_imap_result(self.imap_conn.select(mailbox.encode('mod-utf-7'), True))
         elif response != 'OK':
             raise Execption('Invalid response from IMAP server: %s' % str(data))
 
-    def __cleanFeedMailbox(self, mailbox):
+    def __clean_feed_mailbox(self, mailbox):
         # Delete oldest messages if mailbox contains more than the limit
-        self.__checkImapResult(self.imapConn.select(mailbox.encode('mod-utf-7')))
-        if self.msgLimit > 0:
-            messages = self.__checkImapResult(self.imapConn.sort('DATE', 'ASCII', 'UNDELETED'))[0].split(' ')
-            if len(messages) > self.msgLimit:
-                self.__checkImapResult(self.imapConn.store(','.join(messages[:-self.msgLimit]),
+        self.__check_imap_result(self.imap_conn.select(mailbox.encode('mod-utf-7')))
+        if self.msg_limit > 0:
+            messages = self.__check_imap_result(self.imap_conn.sort('DATE', 'ASCII', 'UNDELETED'))[0].split(' ')
+            if len(messages) > self.msg_limit:
+                self.__check_imap_result(self.imap_conn.store(','.join(messages[:-self.msg_limit]),
                                        '+FLAGS', '\\Deleted'))
-        self.__checkImapResult(self.imapConn.expunge())
-        self.__checkImapResult(self.imapConn.select(mailbox.encode('mod-utf-7'), True))
+        self.__check_imap_result(self.imap_conn.expunge())
+        self.__check_imap_result(self.imap_conn.select(mailbox.encode('mod-utf-7'), True))
     
-    def __getNewestMessageDate(self, mailbox, feedurl):
-        data = self.__checkImapResult(self.imapConn.sort(
+    def __get_newest_message_date(self, mailbox, feedurl):
+        data = self.__check_imap_result(self.imap_conn.sort(
             'REVERSE DATE', 'ASCII', '(HEADER "X-FEED-URL" "%s")' % feedurl))
         mids = string.split(data[0])
-        if len(mids) > 0:
-            data = self.__checkImapResult(self.imapConn.fetch(
+        if mids:
+            data = self.__check_imap_result(self.imap_conn.fetch(
                 mids[0], '(BODY[HEADER.FIELDS (DATE)])'))
             return time.strptime(data[0][1].strip()+' GMT',
                                  "Date: %a, %d %b %Y %H:%M:%S +0000 %Z")
         else:
             return time.gmtime(0)                                                     
     
-    def __getMessageId(self, mailbox, entryUrl):
-        data = self.__checkImapResult(self.imapConn.search(
+    def __get_message_id(self, mailbox, entryUrl):
+        data = self.__check_imap_result(self.imap_conn.search(
                 'ASCII', '(HEADER "MESSAGE-ID" "<%s@localhost.localdomain>")' % entryUrl))
         if data[0] != '':
             return data[0].split(' ')[-1]
         else:
             return None
     
-    def __createMimeMessage(self, feed, entry, created_date=None):
+    def __create_mime_message(self, feed, entry, created_date=None):
         entry_date = time.strftime("%a, %d %b %Y %H:%M:%S +0000",
                                    (entry.has_key('updated_parsed') and entry.updated_parsed
                                     or time.gmtime()))
@@ -224,22 +233,22 @@ X-Feed-Url: %(feedurl)s
     <iframe id="msgIframe" selected="false" src="%(link)s">
   </body>
 </html>
-""" % {'date':created_date != None and created_date or entry_date,
+""" % {'date':created_date is not None and created_date or entry_date,
        'link':entry.link,
        'author':(entry.has_key('author') and '%s <void@feeds2imap>' % entry.author
                  or '%s <void@feeds2imap>' % feed.data.feed.title),
        'title':entry.title,
        'summary':entry.has_key('summary') and entry.summary or '',
        'feedurl':feed.url,
-       'lastupdated':created_date != None and "X-Feed-Lastupdated: %s" % entry_date or ''}
+       'lastupdated':created_date is not None and "X-Feed-Lastupdated: %s" % entry_date or ''}
         
         # Save message to IMAP server
-        self.__checkImapResult(self.imapConn.append(
+        self.__check_imap_result(self.imap_conn.append(
                 feed.mailbox.encode('mod-utf-7'),
                 None, None, message.replace("\n", "\r\n").encode('utf-8')))
     
-    def __checkImapResult(self, result, goodResponse=['OK']):
-        if result[0] in goodResponse:
+    def __check_imap_result(self, result, good_response=['OK']):
+        if result[0] in good_response:
             return result[1]
         else:
             raise Exception('Invalid response from IMAP server: %s' % str(result[1]))
@@ -271,10 +280,10 @@ if __name__=="__main__":
         
         server = unicode(xpath.Evaluate('/feeds2imap/imap/server/child::text()', doc)[0].nodeValue)
         port = int(xpath.Evaluate('/feeds2imap/imap/port/child::text()', doc)[0].nodeValue)
-        useSSL = bool(xpath.Evaluate('/feeds2imap/imap/ssl', doc))
+        ssl = bool(xpath.Evaluate('/feeds2imap/imap/ssl', doc))
         username = unicode(xpath.Evaluate('/feeds2imap/imap/username/child::text()', doc)[0].nodeValue)
         password = unicode(xpath.Evaluate('/feeds2imap/imap/password/child::text()', doc)[0].nodeValue)
-        messagesPerMailbox = int(xpath.Evaluate('/feeds2imap/imap/messagespermailbox/child::text()', doc)[0].nodeValue)
+        messages_per_mailbox = int(xpath.Evaluate('/feeds2imap/imap/messagespermailbox/child::text()', doc)[0].nodeValue)
         
         feeds = []
         for node in xpath.Evaluate('/feeds2imap/feeds/feed', doc):
@@ -286,7 +295,7 @@ if __name__=="__main__":
         sys.exit(1)
     
     reader = FeedReader(feeds, verbose,
-                        server, useSSL, port,
+                        server, ssl, port,
                         username, password,
-                        messagesPerMailbox)
+                        messages_per_mailbox)
     reader.start()
